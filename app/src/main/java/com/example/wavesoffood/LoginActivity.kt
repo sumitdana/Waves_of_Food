b835d182d831f64ae22673edfd03acd5ed2e45d1
sumitdana <sumitdana21@gmail.com>
2025-06-22 22:42:45 +0530

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/com/example/wavesoffood/LoginActivity.kt
+++ app/src/main/java/com/example/wavesoffood/LoginActivity.kt
@@ -82,7 +82,7 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
